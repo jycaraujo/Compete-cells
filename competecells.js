45afc74dfa704c5c3e11e04cbdb2b77170dce1a7
jycaraujo <joycerj93@gmail.com>
2019-01-15 22:43:21 -0200

    Update competecells.js
    description

--- competecells.js
+++ competecells.js
@@ -1,3 +1,9 @@
+// Given a binary array of size n where n > 3. A true (or 1) value in the array means active and false (or 0) means inactive. 
+// Given a number k, the task is to find count of active and inactive cells after k days. After every day, status of i’th cell 
+// becomes active if left and right cells are not same and inactive if left and right cell are same (both 0 or both 1).
+// Since there are no cells before leftmost and after rightmost cells, the value cells before leftmost and after rightmost cells
+// is always considered as 0 (or inactive).
+
 function compete(cells, days){
    while(days--){
       let temp = [];
