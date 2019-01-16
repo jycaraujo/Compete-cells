284e11db6d06b098b6e5bf23964a3ceb41bdb02d
jycaraujo <joycerj93@gmail.com>
2019-01-15 22:40:11 -0200

    Update competecells.js
    small improvement

--- competecells.js
+++ competecells.js
@@ -1,14 +1,15 @@
 function compete(cells, days){
-   let temp = [];
-   temp[0] = cells[1]^0;
-   temp[cells.length-1] = cells[cells.length-2] ^ 0;
    while(days--){
+      let temp = [];
+      temp[0] = cells[1]^0;
+      temp[cells.length-1] = cells[cells.length-2] ^ 0;
       for(let i = 1; i < cells.length-1; i++){
          temp[i] = cells[i-1] ^ cells[i+1];
       }
+      cells = temp
       write(temp);
    }
    
    
 }
-compete([1,1,1,1], 1);
+compete([1,1,1,1], 2);
